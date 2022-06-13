fe12d94624d2fff946304eed71f9b5d50483cbf3
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-06-13 13:39:48 +0300

    fix: class User. remove extra code.

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,23 +1,3 @@
 export class User {
-    private readonly id: number;
-    private readonly name: string;
-    private readonly youtubeUrl: string;
-
-    constructor(id: number, name: string, youtubeUrl: string) {
-        this.id = id;
-        this.name = name;
-        this.youtubeUrl = youtubeUrl;
-    };
-
-    get getId(): number {
-        return this.id;
-    }
-
-    get getName(): string {
-        return this.name;
-    }
-
-    get getYoutubeUrl(): string {
-        return this.youtubeUrl;
-    }
+    constructor(readonly id: number, readonly name: string, readonly youtubeUrl: string) {};
 }
