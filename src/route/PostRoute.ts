c4b4d193308c8ad586d1d01d9496389bd9597db6
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-06-15 17:38:03 +0300

    fix bug.

--- src/route/PostRoute.ts
+++ src/route/PostRoute.ts
@@ -15,7 +15,7 @@ export const postRoute = Router({});
 
 const toPostDTO = (post: Post) => {
     const bloggerId: number | undefined = (!post.blogger) ? -1 : post.blogger.id;
-    const bloggerName: string | undefined= (!post.blogger) ? undefined : post.blogger.name;
+    const bloggerName: string | undefined = (!post.blogger) ? undefined : post.blogger.name;
     return new PostDTO(post.id, post.title, post.shortDescription, post.content, bloggerId, (!bloggerName) ? "" : bloggerName);
 };
 
@@ -75,23 +75,25 @@ postRoute.post("/", postValidator, (req: Request, res: Response) => {
 postRoute.put("/:id", postValidator, (req: Request, res: Response) => {
     const id: number = +req.params.id;
 
-    let post : Post | null = postDAO.findById(id);
+    let post: Post | null = postDAO.findById(id);
     if (!post) {
         res.status(404).send(`Not found post by id = ${id}`);
         return;
     }
 
     const bloggerId: number = +req.body.bloggerId;
-    const blogger: User | null = bloggerDAO.findById(bloggerId);
-    if (!Object.is(post.blogger, blogger)) {
-        res.status(400).send(invalidExistBloggerMessage);
-        return;
-    }
+    /*    const blogger: User | null = bloggerDAO.findById(bloggerId);
+
+        if (!Object.is(post.blogger, blogger)) {
+            res.status(400).send(invalidExistBloggerMessage);
+            return;
+        }
+    */
 
     const title: string = req.body.title;
     const shortDescription: string = req.body.shortDescription;
     const content: string = req.body.content;
-    post = postDAO.update(new Post(id, title, shortDescription, content, blogger));
+    post = postDAO.update(new Post(id, title, shortDescription, content, new User(bloggerId, req.body.bloggerName)));
 
     res.status(204).send(toPostDTO(post));
     return;
