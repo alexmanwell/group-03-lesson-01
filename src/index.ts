d17cd0c4cd739c701cb218906335235ea7eb933b
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-06-15 18:47:29 +0300

    fix bug.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import {bloggerRoute} from "./route/BloggerRoute";
 import {videosRoute} from "./route/VideosRoute";
-import {postRoute} from "./route/PostRoute";
+import {postRoute} from "./route/PostRouteAnotherImpl";
 
 const app = express();
 
--- src/route/PostRouteAnotherImpl.ts
+++ src/route/PostRouteAnotherImpl.ts
@@ -1,29 +1,13 @@
 import {BloggerDAO} from "../repository/BloggerDAO";
-import {PostDAO} from "../repository/PostDAO";
 import {BloggerInMemoryImpl} from "../repository/BloggerInMemoryImpl";
-import {PostInMemoryImpl} from "../repository/PostInMemoryImpl";
+import {postRepository} from "../repository/PostInMemoryRepository";
 import {Request, Response, Router} from "express";
 import {User} from "../model/User";
 import {postValidator} from "../middleware/validate/PostValidator";
-import {Post} from "../model/Post";
-import {PostDTO} from "../model/PostDTO";
 
 const bloggerDAO: BloggerDAO = new BloggerInMemoryImpl();
-const postDAO: PostDAO = new PostInMemoryImpl();
 
 export const postRoute = Router({});
-
-const toPostDTO = (post: Post) => {
-    const bloggerId: number | undefined = (!post.blogger) ? -1 : post.blogger.id;
-    const bloggerName: string | undefined = (!post.blogger) ? undefined : post.blogger.name;
-    return new PostDTO(post.id, post.title, post.shortDescription, post.content, bloggerId, (!bloggerName) ? "" : bloggerName);
-};
-
-const toPostsDTO = (posts: ReadonlyArray<Post>) => {
-    return posts.map((post: Post) => {
-        return toPostDTO(post);
-    });
-};
 
 const invalidExistBloggerMessage = {
     "errorsMessages": [
@@ -35,20 +19,20 @@ const invalidExistBloggerMessage = {
 };
 
 postRoute.get("/", (req: Request, res: Response) => {
-    const posts = postDAO.findAll();
+    const posts = postRepository.findAll();
     res.status(200);
-    res.send(toPostsDTO(posts));
+    res.send(posts);
 });
 
 postRoute.get("/:id", (req: Request, res: Response) => {
     const id: number = +req.params.id;
-    const post = postDAO.findById(id);
+    const post = postRepository.findById(id);
     if (!post) {
         res.sendStatus(404);
         return;
     }
 
-    res.status(200).send(toPostDTO(post));
+    res.status(200).send(post);
     return;
 });
 
@@ -62,20 +46,20 @@ postRoute.post("/", postValidator, (req: Request, res: Response) => {
     const title: string = req.body.title;
     const shortDescription: string = req.body.shortDescription;
     const content: string = req.body.content;
-    const post: Post | null = postDAO.create(new Post(-1, title, shortDescription, content, blogger));
+    const post = postRepository.create([title, shortDescription, content, blogger.id], blogger);
     if (!post) {
         res.sendStatus(404);
         return;
     }
 
-    res.status(201).send(toPostDTO(post));
+    res.status(201).send(post);
     return;
 });
 
 postRoute.put("/:id", postValidator, (req: Request, res: Response) => {
     const id: number = +req.params.id;
 
-    let post: Post | null = postDAO.findById(id);
+    let post = postRepository.findById(id);
     if (!post) {
         res.status(404).send(`Not found post by id = ${id}`);
         return;
@@ -84,23 +68,23 @@ postRoute.put("/:id", postValidator, (req: Request, res: Response) => {
     const bloggerId: number = +req.body.bloggerId;
     const blogger: User | null = bloggerDAO.findById(bloggerId);
 
-        if (!Object.is(post.blogger, blogger)) {
-            res.status(400).send(invalidExistBloggerMessage);
-            return;
-        }
+    if (!blogger) {
+        res.status(400).send(invalidExistBloggerMessage);
+        return;
+    }
 
     const title: string = req.body.title;
     const shortDescription: string = req.body.shortDescription;
     const content: string = req.body.content;
-    post = postDAO.update(new Post(id, title, shortDescription, content, new User(bloggerId, req.body.bloggerName)));
+    post  = postRepository.update({"title": title, "shortDescription": shortDescription, "content": content}, bloggerId);
 
-    res.status(204).send(toPostDTO(post));
+    res.status(204).send(post);
     return;
 });
 
 postRoute.delete('/:id', (req: Request, res: Response) => {
     const id: number = +req.params.id;
-    const isRemovePost: boolean = postDAO.delete(id);
+    const isRemovePost: boolean = postRepository.delete(id);
     if (!isRemovePost) {
         res.sendStatus(404);
         return;
